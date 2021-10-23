bf4cf83b6fb038ac133f3449d68879edd488d463
Bekzod Qalandarov <bekzodqalandarov1994@gmail.com>
2021-10-23 10:25:52 +0500

    refactor selfieActivity

--- myverdisdk/src/main/java/uz/click/myverdisdk/impl/selfie/SelfieActivity.kt
+++ myverdisdk/src/main/java/uz/click/myverdisdk/impl/selfie/SelfieActivity.kt
@@ -2,25 +2,16 @@ package uz.click.myverdisdk.impl.selfie
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.res.Configuration
-import android.graphics.Bitmap
-import android.hardware.display.DisplayManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.DisplayMetrics
 import android.util.Log
 import android.util.Size
-import android.view.KeyEvent
-import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.camera.core.*
 import androidx.camera.core.impl.utils.executor.CameraXExecutors
@@ -28,42 +19,26 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-import androidx.core.os.bundleOf
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.airbnb.lottie.LottieAnimationView
-import com.google.android.material.button.MaterialButton
 import uz.click.myverdisdk.R
 import uz.click.myverdisdk.core.VerdiUser
-import uz.click.myverdisdk.core.VerdiUserConfig
-import uz.click.myverdisdk.core.callbacks.VerdiScanListener
-import uz.click.myverdisdk.core.callbacks.VerdiSelfieListener
-import uz.click.myverdisdk.impl.scan.ScanActivity
-import uz.click.myverdisdk.model.PersonDetails
-import uz.click.myverdisdk.util.GraphicOverlay
 import uz.click.myverdisdk.util.rotateImage
-import uz.click.myverdisdk.util.simulateClick
 import uz.click.myverdisdk.util.toBitmap
-import java.nio.ByteBuffer
-import java.util.ArrayDeque
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-typealias LumaListener = (luma: Double) -> Unit
-
 class SelfieActivity : AppCompatActivity() {
     val KEY_EVENT_ACTION = "key_event_action"
     val KEY_EVENT_EXTRA = "key_event_extra"
 
     lateinit var previewView: PreviewView
-    lateinit var graphicOverlay: GraphicOverlay
     lateinit var ivCameraSwitch: ImageView
     lateinit var ibCameraCapture: ImageButton
     lateinit var llGifView: LinearLayout
 
-    //    lateinit var lottieSelfieAnim: LottieAnimationView
     lateinit var btnOk: AppCompatButton
     lateinit var cameraContainer: ConstraintLayout
 
@@ -73,7 +48,6 @@ class SelfieActivity : AppCompatActivity() {
     private var lensFacing: Int = CameraSelector.LENS_FACING_FRONT
     private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
-    private var imageAnalyzer: ImageAnalysis? = null
     private var camera: Camera? = null
     private var cameraProvider: ProcessCameraProvider? = null
     private lateinit var cameraExecutor: ExecutorService
@@ -81,7 +55,7 @@ class SelfieActivity : AppCompatActivity() {
 
     companion object {
         const val VERDI_LISTENER = "verdiListener"
-        private const val TAG = "CameraXBasic"
+        private const val TAG = "SelfieActivity"
         private const val RATIO_4_3_VALUE = 4.0 / 3.0
         private const val RATIO_16_9_VALUE = 16.0 / 9.0
         fun getInstance(
@@ -97,68 +71,21 @@ class SelfieActivity : AppCompatActivity() {
 
         cameraContainer = findViewById<ConstraintLayout>(R.id.cameraContainer)
         previewView = findViewById<PreviewView>(R.id.previewView)
-        graphicOverlay = findViewById<GraphicOverlay>(R.id.graphicOverlay)
         ivCameraSwitch = findViewById<ImageView>(R.id.cameraSwitchButton)
         ibCameraCapture = findViewById<ImageButton>(R.id.ibCameraCapture)
-//        llGifView = findViewById<LinearLayout>(R.id.llGifView)
-//        lottieSelfieAnim = findViewById<LottieAnimationView>(R.id.lottieSelfieAnim)
 
         viewFinder = previewView
-
         cameraExecutor = Executors.newSingleThreadExecutor()
-
-        broadcastManager = LocalBroadcastManager.getInstance((cameraContainer).context)
-
-        val filter = IntentFilter().apply { addAction(KEY_EVENT_ACTION) }
-        broadcastManager.registerReceiver(volumeDownReceiver, filter)
-
-        displayManager.registerDisplayListener(displayListener, null)
         viewFinder.post {
-
             displayId = viewFinder.display.displayId
             updateCameraUi()
             setUpCamera()
         }
     }
 
-
-    private val displayManager by lazy {
-        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
-    }
-
-
-    private val volumeDownReceiver = object : BroadcastReceiver() {
-        override fun onReceive(contsext: Context, intent: Intent) {
-            when (intent.getIntExtra(KEY_EVENT_EXTRA, KeyEvent.KEYCODE_UNKNOWN)) {
-                KeyEvent.KEYCODE_VOLUME_DOWN -> {
-                    val shutter = ibCameraCapture
-                    shutter.simulateClick()
-                }
-            }
-        }
-    }
-
-    private val displayListener = object : DisplayManager.DisplayListener {
-        override fun onDisplayAdded(displayId: Int) = Unit
-        override fun onDisplayRemoved(displayId: Int) = Unit
-        override fun onDisplayChanged(displayId: Int) = imageCapture?.let { view ->
-            if (displayId == windowManager.defaultDisplay.displayId) {
-                imageCapture?.targetRotation = windowManager.defaultDisplay?.rotation ?: 0
-                imageAnalyzer?.targetRotation = windowManager.defaultDisplay?.rotation ?: 0
-            }
-        } ?: Unit
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
-        broadcastManager.unregisterReceiver(volumeDownReceiver)
-        displayManager.unregisterDisplayListener(displayListener)
-    }
-
-
-    fun initUI() {
-
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -210,30 +137,16 @@ class SelfieActivity : AppCompatActivity() {
             .build()
 
         // ImageCapture
-
         imageCapture = ImageCapture.Builder()
             .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
             .setTargetResolution(Size(1920, 1080))
             .setTargetRotation(rotation)
             .build()
 
-
-        imageAnalyzer = ImageAnalysis.Builder()
-            .setTargetAspectRatio(screenAspectRatio)
-            .setTargetRotation(rotation)
-            .build()
-            .also {
-                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                    Log.d(TAG, "Average luminosity: $luma")
-                })
-            }
-
         cameraProvider.unbindAll()
 
         try {
-            camera = cameraProvider.bindToLifecycle(
-                this, cameraSelector, preview, imageCapture, imageAnalyzer
-            )
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
             preview?.setSurfaceProvider(viewFinder.surfaceProvider)
         } catch (exc: Exception) {
             Log.e(TAG, "Use case binding failed", exc)
@@ -294,7 +207,6 @@ class SelfieActivity : AppCompatActivity() {
     }
 
     private fun updateCameraSwitchButton() {
-
         try {
             ivCameraSwitch.isEnabled = hasBackCamera() && hasFrontCamera()
         } catch (exception: CameraInfoUnavailableException) {
@@ -311,53 +223,4 @@ class SelfieActivity : AppCompatActivity() {
     private fun hasFrontCamera(): Boolean {
         return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
     }
-
-    private class LuminosityAnalyzer(listener: LumaListener? = null) : ImageAnalysis.Analyzer {
-        private val frameRateWindow = 8
-        private val frameTimestamps = ArrayDeque<Long>(5)
-        private val listeners = ArrayList<LumaListener>().apply { listener?.let { add(it) } }
-        private var lastAnalyzedTimestamp = 0L
-        var framesPerSecond: Double = -1.0
-            private set
-
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        override fun analyze(image: ImageProxy) {
-            // If there are no listeners attached, we don't need to perform analysis
-            if (listeners.isEmpty()) {
-                image.close()
-                return
-            }
-
-            // Keep track of frames analyzed
-            val currentTime = System.currentTimeMillis()
-            frameTimestamps.push(currentTime)
-
-            while (frameTimestamps.size >= frameRateWindow) frameTimestamps.removeLast()
-            val timestampFirst = frameTimestamps.peekFirst() ?: currentTime
-            val timestampLast = frameTimestamps.peekLast() ?: currentTime
-            framesPerSecond = 1.0 / ((timestampFirst - timestampLast) /
-                    frameTimestamps.size.coerceAtLeast(1).toDouble()) * 1000.0
-
-            lastAnalyzedTimestamp = frameTimestamps.first
-
-            val buffer = image.planes[0].buffer
-
-            val data = buffer.toByteArray()
-
-            val pixels = data.map { it.toInt() and 0xFF }
-
-            val luma = pixels.average()
-
-            listeners.forEach { it(luma) }
-
-            image.close()
-        }
-    }
-
 }
